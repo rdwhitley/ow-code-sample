324292acfb07da1d01156f44c77cec6ca79ea575
Ubuntu <ubuntu@ip-172-31-93-60.ec2.internal>
2022-10-09 02:02:53 +0000

    test added for formatting hash to valid schema and helper method added for test

--- app/lib/data_transform_service.rb
+++ app/lib/data_transform_service.rb
@@ -23,13 +23,27 @@ class DataTransformService
     row_counter = 0
 
     CSV.open(file_path, 'r:UTF-8', headers: true) do |csv|
+      # puts csv.first["sku"]
       csv.each do |csv_row|
         Rails.logger.debug("Processing row: #{csv_row}")
-
         row_counter += 1
       end
     end
     row_counter
+  end
+  
+  # Takes a row from the parsed csv and formats to json 
+  def formatHash(row)
+  
+  end
+
+  def getFirstRow(file_path)
+    csv_row = nil
+    CSV.open(file_path, 'r:UTF-8', headers: true) do |csv|
+    csv_row = csv.first
+    end
+    puts csv_row.inspect
+    return csv_row
   end
 
 end
--- spec/lib/data_transform_service_spec.rb
+++ spec/lib/data_transform_service_spec.rb
@@ -1,16 +1,41 @@
 require 'rails_helper'
-
+require "json-schema"
 RSpec.describe DataTransformService do
 
   let!(:csv_path) { 'spec/csv/sample-part-data.csv'}
   let!(:service) { DataTransformService.new}
-
+  let!(:schema) { schema = {
+    "type" => "object",
+    "required" => ["a"],
+    "properties" => {
+      "sku" => {"type" => "integer"},
+      "display_pn" => {"type" => "string"},
+      "be_product_cat_name" => {"type" => "string"},
+      "short_description" => {"type" => "string"},
+      "lead_time" => {"type" => "integer"},
+      "meta_description" => {"type" => "string"},
+      "meta_title" => {"type" => "string"},
+      "part_status" => {"type" => "string"},
+      "images" => {"type" => "array", "items" => {"type" => "string"}},
+      "attributes" => {"type" => "array", "items" => {"type" => "string"}}
+       }
+      }
+    }
   describe '#transform' do
 
     it 'should transform rows in the passed file from csv to JSON docs' do
       result = service.transform(csv_path)
       expect(result).to eql(25)
 
+    end
+
+  end
+  
+  describe '#formatHash' do
+
+    it 'should format a given row to the expected hash format' do
+      row = service.getFirstRow(csv_path)
+      expect(JSON::Validator.validate(schema, service.formatHash(row))).to eql(true)
     end
 
   end
